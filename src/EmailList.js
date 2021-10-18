46b4d97a11678ae2888a6a33cea16f77c4b774c4
Lukasz Konatowski <konat1993@gmail.com>
2021-10-19 01:29:22 +0200

    footer created react router applied

---
+++ src/EmailList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import "./EmailList.scss"
+export const EmailList = () => {
+    return (
+        <div className="emailList">
+            EMAILLIST
+        </div>
+    )
+}
+
+export default EmailList
---
+++ src/Mail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import "./Mail.scss"
+export const Mail = () => {
+    return (
+        <div className="mail">
+            Mail
+        </div>
+    )
+}
+
+export default Mail
